35e113c13b1c305f0d86b1e78e8a03d2449caa14
Vinícius Marcili <vinipetter@gmail.com>
2021-01-29 15:40:55 -0300

    Change default Nextpage to some blank page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,23 @@
-import { Component } from 'react'
-import '../../styles/globals.css'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import GlobalStyles from 'styles/global'
 
-type Props = {
-  Component: typeof Component
-  pageProps: { foo: string }
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>NextJS - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-function MyApp({ Component, pageProps }: Props) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
+export default App
